279fa95cbc6b82ac7f470e1e2cebfcb5e4d03060
Jason Johnson <jvsonjohnson@gmail.com>
2020-02-25 23:39:09 -0500

    created first migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,16 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+app.config["SECRET_KEY"] = "$3cr3tk3y"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://lab5:lab5@localhost/lab5"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True  # added just to suppress a warning
 
 db = SQLAlchemy(app)
 
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 
 app.config.from_object(__name__)
 from app import views
